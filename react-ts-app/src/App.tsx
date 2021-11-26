09ad13135567ecc8f574db918d5de946ce7b58af
staunch0515 <staunch0515@gmail.com>
2021-11-27 01:35:16 +0900

    apply for brace new line

--- react-ts-app/src/App.tsx
+++ react-ts-app/src/App.tsx
@@ -8,42 +8,45 @@ import ButtonPage from './mui/button'
 import HomePage from './page/home'
 
 
-function usePageViews() {
-  let location = useLocation();
-  React.useEffect(() => {
-    console.log('page  ' + location.pathname)
-  }, [location])
+function usePageViews()
+{
+	let location = useLocation();
+	React.useEffect(() =>
+	{
+		console.log('page  ' + location.pathname)
+	}, [location])
 }
 
-function App() {
-  usePageViews();
-  return (
-    <div className="App">
-      <header className="App-header">
+function App()
+{
+	usePageViews();
+	return (
+		<div className="App">
+			<header className="App-header">
 
-          <div>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/button">Button</Link>
-              </li>
-            </ul>
+				<div>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/button">Button</Link>
+						</li>
+					</ul>
 
-            <hr />
-            <Switch>
-              <Route exact path="/">
-                <HomePage></HomePage>
-              </Route>
-              <Route path="/button">
-                <ButtonPage></ButtonPage>
-              </Route>
-            </Switch>
-          </div>
-      </header>
-    </div>
-  );
+					<hr />
+					<Switch>
+						<Route exact path="/">
+							<HomePage></HomePage>
+						</Route>
+						<Route path="/button">
+							<ButtonPage></ButtonPage>
+						</Route>
+					</Switch>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 
--- react-ts-app/src/page/home.tsx
+++ react-ts-app/src/page/home.tsx
@@ -1,104 +1,118 @@
 import React, { memo, useCallback, useState, useRef, useLayoutEffect, useEffect, useMemo } from 'react';
 
 type ChildProps = {
-    clickChild: any,
+	clickChild: any,
 }
 
-const PureChild = ({ clickChild }: ChildProps) => {
-    console.log('Child render >>');
-    return <>
-        <div >
-            <button onClick={clickChild} >clickChild</button>
-        </div>
-    </>;
+const PureChild = ({ clickChild }: ChildProps) =>
+{
+	console.log('Child render >>');
+	return <>
+		<div >
+			<button onClick={clickChild} >clickChild</button>
+		</div>
+	</>;
 };
 
 const Child = memo(PureChild);
 
-const Parent = () => {
-    const [count, setCount] = useState(0);
+const Parent = () =>
+{
+	const [count, setCount] = useState(0);
 
-    const h1Ref = useRef<HTMLDivElement>(null);
-
-
-    const clickChild = useCallback((e) => {
-        console.log('click Child');
-
-    }, []);
-
-    useEffect(() => {
+	const h1Ref = useRef<HTMLDivElement>(null);
 
 
-        console.log(' useEffect');
+	const clickChild = useCallback((e) =>
+	{
+		console.log('click Child');
 
-    });
+	}, []);
 
-    function changeInnerText(el: HTMLDivElement, value: string) {
-        el.innerText = value;
-    }
-
-    useLayoutEffect(() => {
-
-        console.log(' useLayoutEffect');
-        if (null !== h1Ref.current) {
-            changeInnerText(h1Ref.current, 'Hello world!' + String(count));
-        }
-
-    }, [count])
+	useEffect(() =>
+	{
 
 
-    const clickParent = () => {
-        console.log('click Parent');
-        setCount(preCount => preCount + 1);
-    };
- 
-    return (
-        <div>
-            <h1 ref={h1Ref}>App{count}</h1>
-            <button onClick={clickParent}>click Parent</button>
-            <Child clickChild={clickChild} />
-        </div>
-    );
+		console.log(' useEffect');
+
+	});
+
+	function changeInnerText(el: HTMLDivElement, value: string)
+	{
+		el.innerText = value;
+	}
+
+	useLayoutEffect(() =>
+	{
+
+		console.log(' useLayoutEffect');
+		if (null !== h1Ref.current)
+		{
+			changeInnerText(h1Ref.current, 'Hello world!' + String(count));
+		}
+
+	}, [count])
+
+
+	const clickParent = () =>
+	{
+		console.log('click Parent');
+		setCount(preCount => preCount + 1);
+	};
+
+	return (
+		<div>
+			<h1 ref={h1Ref}>App{count}</h1>
+			<button onClick={clickParent}>click Parent</button>
+			<Child clickChild={clickChild} />
+		</div>
+	);
 };
 
-const TestHook = () => {
-    const [count, setCount] = useState(0);
+const TestHook = () =>
+{
+	const [count, setCount] = useState(0);
 
-    const isEvenNumber = useMemo(() => {
-        console.log('isEvenNumber');
-        return count % 2 === 0;
-    }, [count]);
+	const isEvenNumber = useMemo(() =>
+	{
+		console.log('isEvenNumber');
+		return count % 2 === 0;
+	}, [count]);
 
-    const isEvenNumber2 = () => {
-        console.log('isEvenNumber2');
-        return count % 2 === 0;
-    };
+	const isEvenNumber2 = () =>
+	{
+		console.log('isEvenNumber2');
+		return count % 2 === 0;
+	};
 
-    const onClick = useCallback(() => {
-        console.log('onClick');
-        setCount(preCount => {
-            return preCount + 1;
-        });
-    }, [count]);
+	const onClick = useCallback(() =>
+	{
+		console.log('onClick');
+		setCount(preCount =>
+		{
+			return preCount + 1;
+		});
+	}, [count]);
 
-    console.log('re-render');
+	console.log('re-render');
 
-    return (
-        <div>
-            <div>{count} is {isEvenNumber2() ? 'even' : 'odd'} number</div>
-            <button onClick={onClick}>Click Me</button>
-        </div>
-    );
+	return (
+		<div>
+			<div>{count} is {isEvenNumber2() ? 'even' : 'odd'} number</div>
+			<button onClick={onClick}>Click Me</button>
+		</div>
+	);
 };
 
 
-export default function Home() {
-    return (
-        <div>
-            <h2>Home</h2>
-            <Parent></Parent>
-            <h2>TestHook</h2>
-            <TestHook></TestHook>
-        </div>
-    );
+export default function Home()
+{
+	return (
+		<div>
+			<h2>Home</h2>
+			<Parent></Parent>
+			<h2>TestHook</h2>
+			<TestHook></TestHook>
+		</div>
+	);
 }
